2fb905c0b7d261e86c1ae768f67d51424a2f61b2
vrk <vrkwork@gmail.com>
2017-06-02 04:27:01 -0700

    add comment

--- cross-stitch-user-login/google-auth.js
+++ cross-stitch-user-login/google-auth.js
@@ -1,5 +1,10 @@
 const GoogleAuth = require('google-auth-library');
 
+// ***NOTE:***
+// You should create your own client ID via these instructions:
+// https://developers.google.com/identity/sign-in/web/devconsole-project
+// ***This client ID WILL BE DELETED by the end of the quarter, so please
+// create your own!!!***
 const CLIENT_ID =
 	'779951118654-9fc4i79pl2qqqet6cs25tbq2ng1dkrip.apps.googleusercontent.com';
 
